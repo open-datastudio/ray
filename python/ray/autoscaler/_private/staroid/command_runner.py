25c794ac22d80d73e3481ee13bcc609da6733282
Lee moon soo <leemoonsoo@gmail.com>
2020-10-03 15:39:33 -0700

    translate exception message

--- python/ray/autoscaler/_private/staroid/command_runner.py
+++ python/ray/autoscaler/_private/staroid/command_runner.py
@@ -1,4 +1,5 @@
 import os
+from typing import Dict
 from ray.autoscaler._private.command_runner import KubernetesCommandRunner
 
 
@@ -27,6 +28,47 @@ class StaroidCommandRunner(KubernetesCommandRunner):
         if target.startswith("~/"):
             return "/home/ray" + target[1:]
         return target
+
+    def run(
+            self,
+            cmd=None,
+            timeout=120,
+            exit_on_fail=False,
+            port_forward=None,
+            with_output=False,
+            environment_variables: Dict[str, object] = None,
+            run_env="auto",  # Unused argument.
+            ssh_options_override_ssh_key="",  # Unused argument.
+            shutdown_after_run=False,
+    ):
+        try:
+            return super().run(
+                cmd,
+                timeout,
+                exit_on_fail,
+                port_forward,
+                with_output,
+                environment_variables,
+                run_env,
+                ssh_options_override_ssh_key,
+                shutdown_after_run
+            )
+        except BaseException as e:
+            err_msg = str(e)
+
+            # When multi node type, while head node container is creating,
+            # translate exception message to more meaningful
+            # Command 'kubectl -n crv-3009-109-5-944-1633-0-3010 --server http://localhost:53208 exec -it ray-headxmn64 -- bash --login -c -i 'true && source ~/.bashrc && export OMP_NUM_THREADS=1 PYTHONWARNINGS=ignore && (uptime)'' returned non-zero exit status 1.
+            # 2020-10-03 11:53:31,574	INFO command_runner.py:165 -- NodeUpdater: ray-headxmn64: Running kubectl -n crv-3009-109-5-944-1633-0-3010 --server http://localhost:53208 exec -it ray-headxmn64 -- bash --login -c -i 'true && source ~/.bashrc && export OMP_NUM_THREADS=1 PYTHONWARNINGS=ignore && (uptime)'
+            # error: unable to upgrade connection: container not found ("ray-node")
+            if "container not found" in err_msg:
+                raise BaseException("Container creating ...")
+
+            # When node is provisioning
+            if "does not have a host assigned" in err_msg:
+                raise BaseException("Node provisioning ...")
+
+            raise e
 
     def run_rsync_up(self, source, target, options=None):
         target = self._rewrite_target_home_dir(target)
